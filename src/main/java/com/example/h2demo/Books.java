5a5b6df47c482c33f0bc64684d77f311b7dd45b1
MarieD <marie.doare.bzh@gmail.com>
2024-09-11 10:51:19 +0200

    create books class, add constructor & get and setter

---
+++ src/main/java/com/example/h2demo/Books.java
@@ -0,0 +1,90 @@
+package com.example.h2demo;
+
+public class Books {
+    // create instance variables
+    int id;
+    String title;
+    String author;
+    String publishYear;
+    String genre;
+    String description;
+    int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(String publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    int quantity;
+
+    public Books(int id, String author, String title, String publishYear, String genre, int price, String description, int quantity) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.publishYear = publishYear;
+        this.genre = genre;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+    }
+}
+
